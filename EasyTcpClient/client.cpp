d94f0e288e5ffcfd44b0555badde8cb99a04540b
hnuhewen@163.com <henudhwbwcx@126.com>
2022-03-17 20:36:11 +0800

    优化结构数据，加入简单登入登出功能

--- EasyTcpClient/client.cpp
+++ EasyTcpClient/client.cpp
@@ -8,9 +8,33 @@
 
 #pragma comment(lib, "ws2_32.lib")
 
-struct DataPackage {
-	int age;
-	char name[32];
+
+enum CMD {
+	CMD_LOGIN,
+	CMD_LOGINOUT,
+	CMD_ERROR
+};
+
+struct DataHeader {
+	short dataLength;
+	short cmd;
+};
+
+struct LoginResult {
+	int result;
+};
+
+struct LoginOutResult {
+	int result;
+};
+
+struct LoginOut {
+	char userName[32];
+};
+
+struct Login {
+	char userName[32];
+	char passWord[32];
 };
 
 int main() {
@@ -45,16 +69,32 @@ int main() {
 		if (0 == strcmp(cmdBuf, "exit")) {
 			break;
 		}
-		else {
-			send(_sock, cmdBuf, strlen(cmdBuf) + 1, 0);
-		}
+		else if (0 == strcmp(cmdBuf, "login")) {
+			Login login = { "zzr", "999" };
+			DataHeader dh = { sizeof(login), CMD_LOGIN};
+			send(_sock, (const char*)&dh, sizeof(dh), 0);
+			send(_sock, (const char*)&login, sizeof(login), 0);
 
-		// 接受服务器信息
-		char recvBuf[256] = {};
-		int nlen = recv(_sock, recvBuf, 256, 0);
-		if (nlen > 0) {
-			DataPackage *dp = (DataPackage*)recvBuf;
-			printf("接收到数据: 年龄：%d， 姓名：%s\n", dp->age, dp->name);
+			DataHeader retHeader = {};
+			LoginResult loginRet = {};
+			recv(_sock, (char*)&retHeader, sizeof(retHeader), 0);
+			recv(_sock, (char*)&loginRet, sizeof(loginRet), 0);
+			printf("LoginResult: %d\n", loginRet.result);
+		}
+		else if (0 == strcmp(cmdBuf, "logout")) {
+			LoginOut logout = { "zzr" };
+			DataHeader dh = { sizeof(logout), CMD_LOGIN};
+			send(_sock, (const char*)&dh, sizeof(dh), 0);
+			send(_sock, (const char*)&logout, sizeof(logout), 0);
+
+			DataHeader retHeader = {};
+			LoginOutResult logoutRet = {};
+			recv(_sock, (char*)&retHeader, sizeof(retHeader), 0);
+			recv(_sock, (char*)&logoutRet, sizeof(logoutRet), 0);
+			printf("LoginOutResult: %d\n", logoutRet.result);
+		}
+		else {
+			printf("不支持的命令，请重新输入.\n");
 		}
 	}
 	
